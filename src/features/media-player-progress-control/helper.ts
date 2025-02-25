54b8542576d52053973f896b607d74362a7c2709
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-02-25 10:31:37 +0100

    fix: round numbers

--- src/features/media-player-progress-control/helper.ts
+++ src/features/media-player-progress-control/helper.ts
@@ -3,6 +3,6 @@ export function formatDuration(durationSeconds: number | undefined) {
     return "0:00";
   }
   const minutes = Math.floor(durationSeconds / 60);
-  const seconds = durationSeconds % 60;
+  const seconds = Math.floor(durationSeconds % 60);
   return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 }
