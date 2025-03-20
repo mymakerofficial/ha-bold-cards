b3a31099808af33078cffd1aba8198e2e3ef3513
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-03-20 22:58:48 +0100

    chore: removed old types

--- src/cards/at-a-glance-card/types.ts
+++ src/cards/at-a-glance-card/types.ts
@@ -4,9 +4,5 @@ import { GlancePageConfig } from "../../lib/at-a-glance/types";
 
 export interface BoldAtAGlanceCardConfig extends LovelaceCardConfig {
   type: BoldCardTypes["AT_A_GLANCE"];
-  // TODO remove
-  title_template?: string;
-  // TODO remove
-  content_template?: string;
   pages?: GlancePageConfig[];
 }
