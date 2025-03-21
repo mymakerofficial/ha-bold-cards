7f55c06ec4683a62fd0130599130df8fc9fab27b
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-03-21 19:37:46 +0100

    feat: handle weather more info

--- src/cards/weather-card/bold-weather-card.ts
+++ src/cards/weather-card/bold-weather-card.ts
@@ -15,6 +15,13 @@ import {
   getWeatherIcon,
 } from "../../lib/weather/helpers";
 import { HassEntity } from "home-assistant-js-websocket";
+import { actionHandler } from "../../helpers/ha/action-handler-directive";
+import {
+  ActionHandlerEvent,
+  fireEvent,
+  handleAction,
+  hasAction,
+} from "custom-card-helpers";
 
 const MASK_IMAGE = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(`
       <svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
@@ -88,25 +95,97 @@ export class BoldWeatherCard extends BoldCardWithEntity<
     const icon = getWeatherIcon(stateObj.state);
 
     return html`<div class="content">
+      <button
+        class="background"
+        role="button"
+        tabindex="0"
+        @click=${this._handleMoreInfo}
+      >
+        <ha-ripple></ha-ripple>
+      </button>
       <bc-icon class="icon" icon=${icon}></bc-icon>
       <div class="label">${temperature}˚</div>
     </div>`;
   }
 
+  protected _handleMoreInfo() {
+    fireEvent(this, "hass-more-info", {
+      entityId: this._config!.entity,
+    });
+  }
+
   static get styles() {
     return css`
+      [role="button"] {
+        cursor: pointer;
+        pointer-events: auto;
+      }
+
+      [role="button"]:focus {
+        outline: none;
+      }
+
       :host {
         height: 100%;
         width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
+        --state-color: var(--primary-color);
+        --state-icon-color: var(--primary-color);
+        --ha-ripple-color: var(--primary-color);
+        --ha-ripple-hover-opacity: 0.04;
+        --ha-ripple-pressed-opacity: 0.12;
       }
 
       .content {
         height: 100%;
         aspect-ratio: 1 / 1;
         position: relative;
+      }
+
+      .content::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: transparent;
+        -webkit-mask-size: contain;
+        mask-size: contain;
+        -webkit-mask-position: center;
+        mask-position: center;
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-image: url("${unsafeCSS(MASK_IMAGE)}");
+        mask-image: url("${unsafeCSS(MASK_IMAGE)}");
+        transition:
+          background-color 180ms ease-in-out,
+          width 180ms ease-in-out,
+          height 180ms ease-in-out,
+          top 180ms ease-in-out,
+          left 180ms ease-in-out;
+      }
+
+      .content:has(.background:focus-visible)::before {
+        background: var(--primary-color);
+        top: -2px;
+        left: -2px;
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+      }
+
+      .content * {
+        pointer-events: none;
+      }
+
+      .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
         background: var(
           --ha-card-background,
           var(--card-background-color, #fff)
@@ -119,12 +198,15 @@ export class BoldWeatherCard extends BoldCardWithEntity<
         mask-repeat: no-repeat;
         -webkit-mask-image: url("${unsafeCSS(MASK_IMAGE)}");
         mask-image: url("${unsafeCSS(MASK_IMAGE)}");
+        cursor: pointer;
+        pointer-events: all;
+        border: none;
       }
 
       .icon {
         position: absolute;
         bottom: 35%;
-        left: 38%;
+        left: 40%;
         transform: translate(-50%, 50%);
         width: 50%;
         height: 50%;
