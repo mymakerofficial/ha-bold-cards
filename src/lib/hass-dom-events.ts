1852649d361ba0bb5431fc2e571a72f6ff9ce637
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-05-15 22:41:16 +0200

    chore: added showToast method

---
+++ src/lib/hass-dom-events.ts
@@ -0,0 +1,19 @@
+export interface ShowToastParams {
+  // Unique ID for the toast. If a new toast is shown with the same ID as the previous toast, it will be replaced to avoid flickering.
+  id?: string;
+  message: string;
+  action?: ToastActionParams;
+  duration?: number;
+  dismissable?: boolean;
+}
+
+export interface ToastActionParams {
+  action: () => void;
+  text: string;
+}
+
+declare global {
+  interface HASSDomEvents {
+    "hass-notification": ShowToastParams;
+  }
+}
