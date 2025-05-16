4b6c230d0cd99c9349e73d4a7d2296b03ec232c2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-05-16 20:56:25 +0200

    fixup! feat: add battery card placeholder editor

--- src/editors/cards/battery-card/bold-battery-card-editor.ts
+++ src/editors/cards/battery-card/bold-battery-card-editor.ts
@@ -2,13 +2,13 @@ import { html } from "lit";
 import { customElement } from "lit/decorators";
 import { BoldCardType } from "../../../lib/cards/types";
 import { getCardEditorTag } from "../../../lib/cards/helpers";
-import { CarouselCardConfig } from "../../../cards/carousel-card/types";
 import { BoldLovelaceCardEditor } from "../base";
 import { object } from "superstruct";
 import { t } from "../../../localization/i18n";
+import { BatteryCardConfig } from "../../../cards/battery-card/types";
 
 @customElement(getCardEditorTag(BoldCardType.BATTERY))
-export class BoldBatteryCardEditor extends BoldLovelaceCardEditor<CarouselCardConfig> {
+export class BoldBatteryCardEditor extends BoldLovelaceCardEditor<BatteryCardConfig> {
   protected get _struct() {
     return object();
   }
